392fed1047d6746b96c8c3d307d351a86e1fdd95
jyoo0515 <jasonyoo0515@gmail.com>
2021-10-16 16:53:11 +0900

    Minor edit

--- server/users.js
+++ server/users.js
@@ -2,18 +2,7 @@ const xlsx = require("xlsx");
 
 const getUsers = () => {
     const obj = xlsx.readFile(__dirname + "/asset/user_profile.xlsx");
-    // const sheets = obj.SheetNames;
-    // let result = [];
-
-    // for (let i = 0; i < sheets.length; i++) {
-    //     const tmp = xlsx.utils.sheet_to_json(obj.Sheets[obj.SheetNames[i]])
-    //     tmp.forEach((res) => {
-    //         result.push(res);
-    //     });
-    // }
     return xlsx.utils.sheet_to_json(obj.Sheets["user_profile"]);
-
-    // return result;
 };
 
 module.exports = getUsers;
